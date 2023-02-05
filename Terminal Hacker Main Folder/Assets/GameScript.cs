1975d8295c2c2f77780c0439a0217dd7d9e5d79c
Marvin <mjae0109@gmail.com>
2023-02-05 13:31:25 +0800

    go back to menu func added

--- Terminal Hacker Main Folder/Assets/GameScript.cs
+++ Terminal Hacker Main Folder/Assets/GameScript.cs
@@ -1,23 +1,32 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
 
 public class GameScript : MonoBehaviour
 {
+    enum Screen { NameSelect, MainMenu, LocalLibraryLevel}
+    Screen currentScreen;
     string name;
-    int count = 0;
+    string[] arrayUsed;
+    int arrayUsedLen;
+    string[] library = { "books", "pencil", "librarian" };
+
     void Start()
     {
+        currentScreen = Screen.NameSelect;
         AskUserName();
     }
     
     void AskUserName()
     {
-        Terminal.WriteLine("What do you want to be called:");
+        Terminal.WriteLine("What do you want to be called:"); 
     }
 
     void ShowMainMenu()
     {
+        currentScreen = Screen.MainMenu;
         Terminal.ClearScreen();
         Terminal.WriteLine("Hello " + name + ",");
         Terminal.WriteLine("What would you like to hack into?");
@@ -27,12 +36,36 @@ public class GameScript : MonoBehaviour
     }
     void OnUserInput(string input)
     {
-        if (count == 0)
+        if (currentScreen == Screen.NameSelect)
         {
             name = input;
-            count++;
             ShowMainMenu();
         }
+
+        else if (input == "1" & currentScreen == Screen.MainMenu)
+        {
+            LocalLibrary();
+        }
+
+        else if (input == "menu")
+        {
+            ShowMainMenu();
+        }
+    }
+    void WordSelection()
+    {
+        System.Random rd = new System.Random();
+        int word = rd.Next(0, arrayUsedLen);
+        Terminal.WriteLine("Password: " + arrayUsed[word]);
+    }
+    void LocalLibrary()
+    {
+        currentScreen = Screen.LocalLibraryLevel;
+        arrayUsed = library;
+        arrayUsedLen = library.Length;
+        Terminal.ClearScreen();
+        Terminal.WriteLine("Level 1 - Local Library");
+        WordSelection();
 
     }
     
